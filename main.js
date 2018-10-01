385b2891ca13912e8af3171dc6ba80d45900da10
Max Rohde <m.rohde@pureleap.com>
2018-10-01 18:33:55 +1000

    Improve build

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const { app, BrowserWindow } = require('electron');
 
+  app.on('ready', createWindow);
+
   function createWindow () {
     win = new BrowserWindow({ width: 400, height: 335 });
     win.setMenu(null);
     win.loadFile('index.html');
   }
 
-  app.on('ready', createWindow);
+  app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+      app.quit();
+    }
+ });
